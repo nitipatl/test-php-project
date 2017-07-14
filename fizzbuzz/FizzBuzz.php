49a48fef7d238711370f15aed37ec7ea776da58a
iFew <chitpong@gmail.com>
2017-07-14 10:22:31 +0700

    practice from class

--- fizzbuzz/FizzBuzz.php
+++ fizzbuzz/FizzBuzz.php
@@ -1,1 +1,16 @@
 <?php
+function GetNumber($n) {
+	$fizzbuzz =  "FIZZBUZZ";
+	$fizz = "FIZZ";
+	$buzz = "BUZZ";
+	
+	if(($n %3 == 0) && ($n % 5 == 0)) {
+		return $fizzbuzz;
+	}elseif($n % 3 == 0){
+		return $fizz;
+	}elseif($n % 5 == 0) {
+		return $buzz;
+	}
+	
+	return $n;
+}
--- fizzbuzz/TestFizzBuzz.php
+++ fizzbuzz/TestFizzBuzz.php
@@ -1,6 +1,45 @@
 <?php
-include 'FizzBuzz.php';
-
+include ("FizzBuzz.php");
 class TestFizzBuzz extends PHPUnit_Framework_TestCase {
+	function testInput1Return1() {
+		$expected = "1";
+		$input = "1";
+		 
+		$actual = GetNumber($input);
+		
+		$this->assertEquals($expected, $actual);
+	}
+	function testInput2Return2() {
+		$expected = "2";
+		$input = "2";
+		
+		$actual = GetNumber($input);
+		
+		$this->assertEquals($expected, $actual);
+	}
 	
+	function testInput3ReturnFizz() {
+		$expected = "FIZZ";
+		$input = "3";
+		
+		$actual = GetNumber($input);
+		
+		$this->assertEquals($expected, $actual);
+	}
+	function testInput5ReturnBuzz() {
+		$expected = "BUZZ";
+		$input = "5";
+		
+		$actual = GetNumber($input);
+		
+		$this->assertEquals($expected, $actual);
+	}
+	function testInput15ReturnFizzBuzz() {
+		$expected = "FIZZBUZZ";
+		$input = "15";
+		
+		$actual = GetNumber($input);
+		
+		$this->assertEquals($expected, $actual);
+	}
 }
